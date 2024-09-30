55656013d592c3869e2fb1516ce1bf25ee55204f
bufdev <bufdev-github@buf.build>
2024-09-30 11:01:07 -0400

    commit

--- pkg/consume/consumer.go
+++ pkg/consume/consumer.go
@@ -19,7 +19,7 @@ import (
 // a malformed data handler is invoked.
 //
 // This is a toy example, but shows the basics you need to receive Protobuf messages
-// from Kafka using franz-go.
+// from Kafka using franz-go. You can likely use this as a base to build out your own demo.
 type Consumer[M proto.Message] struct {
 	client               *kgo.Client
 	deserializer         serde.Deserializer
@@ -66,12 +66,21 @@ func WithMessageHandler[M proto.Message](messageHandler func(M) error) ConsumerO
 	}
 }
 
-func WithMalformedDataHandler[M proto.Message](malformedDataHandler func([]byte, error) error) ConsumerOption[M] {
+// WithMalformedDataHandler returns a new Consumer Option that overrides the default
+// handler of malformed received data.
+//
+// The default handler uses slog to log the error returned on malformed data.
+func WithMalformedDataHandler[M proto.Message](
+	malformedDataHandler func([]byte, error) error,
+) ConsumerOption[M] {
 	return func(consumer *Consumer[M]) {
 		consumer.malformedDataHandler = malformedDataHandler
 	}
 }
 
+// Consume consumes as many record as it can from the topic, deserializing them into
+// a message of type M if it can, and then invoking the message handler. It invokes the
+// malformed data handler if the record's payload cannot be deserialized into type M.
 func (c *Consumer[M]) Consume(ctx context.Context) error {
 	fetches := c.client.PollFetches(ctx)
 	if errs := fetches.Errors(); len(errs) > 0 {
@@ -87,8 +96,12 @@ func (c *Consumer[M]) Consume(ctx context.Context) error {
 		}
 		message, ok := data.(M)
 		if !ok {
-			// TODO: This was a log only, why? This should never happen in our code.
-			return fmt.Errorf("received unexpected message type: %T", data)
+			if err := c.malformedDataHandler(
+				record.Value,
+				fmt.Errorf("received unexpected message type: %T", data),
+			); err != nil {
+				return err
+			}
 		}
 		if err := c.messageHandler(message); err != nil {
 			return err
