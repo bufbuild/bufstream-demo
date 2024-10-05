90309fdefcce33804e9061e5d7b4c28acc352c1a
alapienebuf <alapiene@buf.build>
2024-10-05 23:18:19 +0300

    Add TLS dialer support for kafka client (#12)
    so it can talk to TLS enabled bufstream

--- pkg/kafka/kafka.go
+++ pkg/kafka/kafka.go
@@ -1,6 +1,11 @@
 package kafka
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/twmb/franz-go/pkg/kgo"
@@ -16,6 +21,7 @@ type Config struct {
 	//
 	// TODO: A good explanation and links to docs as to what this does.
 	BootstrapServers []string
+	RootCAPath       string
 	Group            string
 	Topic            string
 	ClientID         string
@@ -23,12 +29,43 @@ type Config struct {
 
 // NewKafkaClient returns a new franz-go Kafka Client for the given Config.
 func NewKafkaClient(config Config) (*kgo.Client, error) {
-	return kgo.NewClient(
+	opts := []kgo.Opt{
 		kgo.SeedBrokers(config.BootstrapServers...),
 		kgo.ConsumerGroup(config.Group),
 		kgo.ConsumeTopics(config.Topic),
 		kgo.ClientID(config.ClientID),
 		kgo.AllowAutoTopicCreation(),
 		kgo.FetchMaxWait(time.Second),
-	)
+	}
+
+	if config.RootCAPath != "" {
+		dialerTLSConfig, err := buildDialerTLSConfig(config.RootCAPath)
+		if err != nil {
+			return nil, fmt.Errorf("build dial tls config: %w", err)
+		}
+
+		opts = append(opts, kgo.DialTLSConfig(dialerTLSConfig))
+	}
+
+	return kgo.NewClient(opts...)
+}
+
+func buildDialerTLSConfig(rootCAPath string) (*tls.Config, error) {
+	pool := x509.NewCertPool()
+
+	caCert, err := os.ReadFile(rootCAPath)
+	if err != nil {
+		return nil, err
+	}
+
+	if !pool.AppendCertsFromPEM(caCert) {
+		return nil, errors.New("parse CA cert failed")
+	}
+
+	tlsCfg := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+		RootCAs:    pool,
+	}
+
+	return tlsCfg, nil
 }
